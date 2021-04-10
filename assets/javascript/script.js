6ffad6cfb83d21d3a08590b86c5638fd9d9703c1
Teller Wetzel <tellerwetzel@yahoo.com>
2021-04-10 14:20:08 -0600

    have functioning questions

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,39 +1,49 @@
 // // Body element reference
-var timeEl = document.getElementById("time");
-var mainEl = document.getElementsByClassName("welcome");
-var startBtn =document.getElementById("start");
+var timeEl = document.querySelector("#time");
+var mainEl = document.querySelector(".welcome");
+var answerEl = document.querySelector(".info");
+var startBtn = document.querySelector("#start");
+var resultEl = document.querySelector(".result");
+var number = 0;
 var highScores = [];
 var score = 0;
+var game = true;
+var correct = 0;
+var wrong = 0;
+var timeLeft = 45;
 var questions = [
     {
-     question: "Commonly used data types DO Not include?",
-     options: ["Strings", "Booleans", "Alerts", "Numbers"],
-     answer: "3"
+     q: "Commonly used data types DO Not include?",
+     o: ["Strings", "Booleans", "Alerts", "Numbers"],
+     a: "3"
     },
     {
-     question: "A very useful tool used during development and debugging for printing content to the debugger is?",
-     options: ["JavaScrip", "Terminal/Bash", "For Loops", "Console.log"],
-     answer: "4"
+     q: "A very useful tool used during development and debugging for printing content to the debugger is?",
+     o: ["JavaScrip", "Terminal/Bash", "For Loops", "Console.log"],
+     a: "4"
     },
     {
-      question: "String values must be enclosed within ________ when being assigned to variables?",
-      options: ["Commas", "Curly Brackets", "Quotes", "Parenthesis"],
-      answer: "3"
+      q: "String values must be enclosed within ________ when being assigned to variables?",
+      o: ["Commas", "Curly Brackets", "Quotes", "Parenthesis"],
+      a: "3"
     },
     {
-      question: "Arrays in JavaScript can be used to store _________?",
-      options: ["Number and Strings", "Other Arrays", "Booleans", "All Of The Above"],
-      answer: "4"
+      q: "Arrays in JavaScript can be used to store _________?",
+      o: ["Number and Strings", "Other Arrays", "Booleans", "All Of The Above"],
+      a: "4"
     },
     {
-      question: "The condition in an if / else statement is enclosed with _________?",
-      options: ["Quotes", "Curly Brackets", "Parenthesis", "Square Brackets"],
-      answer: "3"
+      q: "The condition in an if / else statement is enclosed with _________?",
+      o: ["Quotes", "Curly Brackets", "Parenthesis", "Square Brackets"],
+      a: "3"
     },
 ]
-
+function startQuiz() {
+    countdown();
+    startBtn.remove();
+    quiz();
+}
 function countdown() {
-    var timeLeft = 45;
     var timeInterval = setInterval(function() {
         if (timeLeft > 1) {
             timeEl.textContent = timeLeft;
@@ -42,16 +52,59 @@ function countdown() {
         else {
             timeEl.textContent = "";
             clearInterval(timeInterval);
+            // endQuiz();
         }
     }, 1000);
 }
 
-var quiz = function(){
-    for (var i = 0; i < questions.length; i++) {
-        var answer = confirm(questions[i].question);
-        
+function quiz() {
+    if (number === questions.length) {
+        game = false;
+    }
+    else {
+    var question = questions[number].q;
+    var options = questions[number].o;
+    answer = questions[number].a;
+    mainEl.textContent = question;
+    answerEl.textContent = "";
+    for (var i = 0; i < options.length; i++) {
+        var buttonEl = document.createElement("button");
+        buttonEl.className = "btn guess";
+        buttonEl.setAttribute("button-id", [i+1]);
+        buttonEl.textContent = `${[i+1]}. ${options[i]}`;
+        answerEl.appendChild(buttonEl);
+    }
+    number++;
+}
+}
+
+answerGuess = function(event) {
+    var targetEl = event.target;
+    if(targetEl.matches(".guess")) {
+        var guessEl = targetEl.getAttribute("button-id");
+        guessCorrect(guessEl);
     }
 }
 
+guessCorrect = function(guessEl) {
+    if(guessEl === answer) {
+        timeLeft = timeLeft + 3;
+        correct++;
+        resultEl.textContent = "Correct!";
+        quiz();
+    }
+    else {
+        timeLeft = timeLeft - 5;
+        wrong++;
+        resultEl.textContent = "Wrong!";
+        quiz();
+    }
+}
 
-startBtn.onclick = quiz;
+endQuiz = function() {
+
+}
+
+
+startBtn.addEventListener("click", startQuiz);
+answerEl.addEventListener("click", answerGuess);
